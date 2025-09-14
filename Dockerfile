c82eb3fb80f3590b7efc8498f66e9e6e554ab295
shota <shotashiina132@gmail.com>
2025-09-15 02:05:34 +0900

    コメントの追加とcheck pointの保存

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
+# python:3.13.7-slim-trixieというimageをもとにimageを構築
 FROM python:3.13.7-slim-trixie
 
+# 以下のコマンドなどの実行を/appディレクトリで行う
 WORKDIR /app
 
+# jupyterlabのインストール
 RUN pip install --upgrade pip
 RUN python -m pip install jupyterlab
 
-
+# ローカルの作業ディレクトリ内のファイルををコピー
 COPY . .
--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -1,10 +1,10 @@
-# 接続を許可するIPアドレス
+# IPアドレス (コンテナ名によるDNS解決を利用)
 c.ServerApp.ip = "app"
 
 # ポート番号
 c.ServerApp.port = 8000
 
-# アクセス時に必要なトークンを無効化
+# アクセス時に必要なトークンを設定
 c.ServerApp.token = "jupyter-password"
 
 # コマンド実行時にブラウザを開かない
@@ -13,13 +13,13 @@ c.ServerApp.open_browser = False
 # ルートユーザーで使用する
 c.ServerApp.allow_root = True
 
-# ルートユーザーのホームディレクトリをJupyterLab上のルートディレクトリに設定
+# JupyterLabのルートディレクトリを /app に設定
 c.ServerApp.root_dir = "/app"
 
-# JupyterLab上のファイルツリーで隠しファイルを表示するオプションを使用可能にする
+# 隠しファイルを表示する
 c.ContentsManager.allow_hidden = True
 
-# チェックポイントファイルを /tmp に保存する
+# チェックポイントファイルを /root/ipynb_checkpoints に保存する
 c.FileContentsManager.checkpoints_kwargs = {
-    "root_dir": "/tmp/ipynb_checkpoints",
+    "root_dir": "/root/ipynb_checkpoints",
 }
